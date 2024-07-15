242dba51928cfd8357a8838620635021aa43e80a
Robert Hapaianu <roberthapaianu@yahoo.com>
2024-07-15 23:10:35 +0300

    Initial commit

---
+++ src/main/java/com/example/ElectivCourses/ElectivCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ElectivCourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectivCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectivCoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ElectivCourses/ElectivCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ElectivCourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectivCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
