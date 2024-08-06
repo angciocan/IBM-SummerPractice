71c46f9a8f6e0d8c96ec84d34c399b553ba8c41d
MirceaTT8 <mircea.munteanu356@gmail.com>
2024-08-06 23:08:03 +0300

    added drag&drop again for electives

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -13,6 +13,8 @@ insert into course(day_of_week,max_students,study_year,time,teacher_id,category,
 insert into course(day_of_week,max_students,study_year,time,teacher_id,category,course_name) values (4,23,3,'17:00',1,'mandatory','programare web');
 insert into course(day_of_week,max_students,study_year,time,teacher_id,category,course_name) values (2,23,3,'17:00',1,'elective','bazele programarii 2');
 insert into course(day_of_week,max_students,study_year,time,teacher_id,category,course_name) values (3,23,3,'17:00',1,'elective','programare web 2');
+insert into course(day_of_week,max_students,study_year,time,teacher_id,category,course_name) values (5,23,3,'17:00',1,'elective','microeconomie');
+insert into course(day_of_week,max_students,study_year,time,teacher_id,category,course_name) values (6,23,3,'17:00',1,'elective','management');
 insert into enrollment_administration(nr_of_mandatory_courses,nr_of_elective_courses, study_year) values (4,2, 2);
 insert into enrollment_administration(nr_of_mandatory_courses,nr_of_elective_courses, study_year) values (3,3, 3);
 insert into enrollment_administration(nr_of_mandatory_courses,nr_of_elective_courses, study_year) values (2,4, 4);
--- frontend/src/app/components/elective/elective.component.ts
+++ frontend/src/app/components/elective/elective.component.ts
@@ -1,28 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import { EnrollmentAdministrationService } from '../../services/enrollment-administration.service';
 import { EnrollmentAdministration } from '../../interfaces/enrollment-administration';
+import {Course} from "../../interfaces/course";
+import {SelectedUserService} from "../../services/selected-user.service";
+import {Student} from "../../interfaces/student";
+import {User} from "../../interfaces/user";
+import {forkJoin, map, switchMap} from "rxjs";
+import {CdkDragDrop, moveItemInArray, DragDropModule, CdkDropList, CdkDrag} from '@angular/cdk/drag-drop';
+import {CourseService} from "../../services/course.service";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-elective',
   standalone: true,
   templateUrl: './elective.component.html',
+  imports: [
+    NgForOf,
+    NgIf,
+    CdkDropList,
+    CdkDrag
+  ],
   styleUrls: ['./elective.component.scss']
 })
 export class ElectiveComponent implements OnInit {
 
   enrollmentAdministrations: EnrollmentAdministration[] = [];
+  electiveCourses: Course[] = [];
   startTime: Date | null = null;
   endTime: Date | null = null;
+  protected selectedUser: User | null = null;
+  private courses: Course[] = [];
+  courseApplicationCounts: { [courseId: number]: number } = {};
 
-  constructor(private enrollmentAdministrationService: EnrollmentAdministrationService) {}
+
+  constructor(
+    private enrollmentAdministrationService: EnrollmentAdministrationService,
+    private selectedUserService: SelectedUserService,
+    private courseService: CourseService,
+    private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
+    // Hardcoded values for setting the enrollment period
+    const hardcodedStartTime = '2024-08-01';
+    const hardcodedEndTime = '2024-08-15';
+
+    // Set the enrollment period with the hardcoded values
+    this.setEnrollmentPeriod(hardcodedStartTime, hardcodedEndTime);
+
     this.enrollmentAdministrationService.getAllEnrollmentAdministration().subscribe({
       next: (enrollments: EnrollmentAdministration[]) => {
-        console.log('Enrollments:', enrollments);
+        // console.log('Enrollments:', enrollments);
         this.enrollmentAdministrations = enrollments;
-        console.log(this.enrollmentAdministrations);
-        this.enrollmentAdministrationService.getEnrollmentPeriod()
       },
       error: (err) => {
         console.error('Error fetching enrollments:', err);
@@ -34,23 +62,59 @@ export class ElectiveComponent implements OnInit {
         if (enrollmentPeriod.length === 2) {
           this.startTime = new Date(enrollmentPeriod[0]);
           this.endTime = new Date(enrollmentPeriod[1]);
-          console.log('Enrollment Period:', this.startTime, this.endTime);
         }
       },
       error: (err) => {
         console.error('Error fetching enrollment period:', err);
       }
     });
+
+    this.selectedUserService.selectedUser$.subscribe(user => {
+      this.selectedUser = user;
+      console.log(this.selectedUser)
+      if (this.selectedUser && ((this.selectedUser as Student).role === 'student')) {
+        this.loadCoursesSameYearForStudent((this.selectedUser as Student).studyYear)
+      }
+    });
+    this.cdr.detectChanges();
+
+  }
+
+  private loadCoursesSameYearForStudent(studyYear: number): void {
+    this.courseService.getCoursesByStudyYear(studyYear).pipe(
+      switchMap(courses => {
+        this.courses=courses
+        this.electiveCourses = this.courses.filter(course => course.category === 'elective');
+        const applicationObservables = this.courses.map(course =>
+          this.courseService.getNrOfApplicationsForCourse(course.id).pipe(
+            map(count => ({ courseId: course.id, count }))
+          )
+        );
+
+        return forkJoin(applicationObservables);
+      })
+    ).subscribe(applicationCounts => {
+      applicationCounts.forEach(appCount => {
+        this.courseApplicationCounts[appCount.courseId] = appCount.count;
+      });
+      console.log(this.electiveCourses)
+    });
   }
 
   setEnrollmentPeriod(startTime: string, endTime: string): void {
     this.enrollmentAdministrationService.setEnrollmentPeriod(startTime, endTime).subscribe({
       next: () => {
-        console.log('Enrollment period set successfully');
+        // console.log('Enrollment period set successfully');
       },
       error: (err) => {
         console.error('Error setting enrollment period:', err);
       }
     });
   }
+
+  drop(event: CdkDragDrop<Course[]>): void {
+    moveItemInArray(this.electiveCourses, event.previousIndex, event.currentIndex);
+    console.log('Elective courses reordered:', this.electiveCourses);
+  }
+
 }
