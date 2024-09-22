149466fb39071fd3e4339e40ad30ba45c4b8af41
MirceaTT8 <mircea.munteanu356@gmail.com>
2024-09-23 00:01:35 +0300

    added event & listener for enrollmentperiod

--- src/main/java/com/example/ElectivCourses/service/impl/EnrollmentPeriodServiceImpl.java
+++ src/main/java/com/example/ElectivCourses/service/impl/EnrollmentPeriodServiceImpl.java
@@ -1,9 +1,11 @@
 package com.example.ElectivCourses.service.impl;
 
 import com.example.ElectivCourses.Model.entity.EnrollmentPeriod;
+import com.example.ElectivCourses.event.EnrollmentPeriodOpenedEvent;
 import com.example.ElectivCourses.repository.EnrollmentPeriodRepository;
 import com.example.ElectivCourses.service.EnrollmentPeriodService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +14,17 @@ public class EnrollmentPeriodServiceImpl implements EnrollmentPeriodService {
     @Autowired
     private EnrollmentPeriodRepository enrollmentPeriodRepository;
 
+    @Autowired
+    private ApplicationEventPublisher eventPublisher;
+
     @Override
     public void openEnrollmentPeriod() {
         EnrollmentPeriod period = enrollmentPeriodRepository.findOrCreateSingleton();
         period.setOpen(true);
         enrollmentPeriodRepository.save(period);
+
+        eventPublisher.publishEvent(new EnrollmentPeriodOpenedEvent(this, period.getId()));
+
     }
 
     @Override
@@ -24,6 +32,8 @@ public class EnrollmentPeriodServiceImpl implements EnrollmentPeriodService {
         EnrollmentPeriod period = enrollmentPeriodRepository.findOrCreateSingleton();
         period.setOpen(false);
         enrollmentPeriodRepository.save(period);
+
+        eventPublisher.publishEvent(new EnrollmentPeriodOpenedEvent(this, period.getId()));
     }
 
     @Override
